83ef74a9666d578b1957be06f171833dc9b42071
Ahmed Shahrour <ahmadshahrour951@gmail.com>
2020-04-10 17:21:27 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -12,11 +12,11 @@ import {
   faPlusCircle,
   faUser,
   faCircle,
-  faBars,
+  faBars
 } from '@fortawesome/free-solid-svg-icons';
 import {
   FontAwesomeIcon,
-  FontAwesomeLayers,
+  FontAwesomeLayers
 } from '@fortawesome/vue-fontawesome';
 
 library.add(
@@ -33,10 +33,36 @@ library.add(
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.component('font-awesome-layers', FontAwesomeLayers);
 
-import { BootstrapVue } from 'bootstrap-vue';
+import {
+  BContainer,
+  BRow,
+  BCol,
+  BNavbar,
+  BLink,
+  BFormCheckbox,
+  BFormRadioGroup,
+  ModalPlugin,
+  BCard,
+  BCardBody,
+  BCardTitle,
+  BTable,
+  BButton
+} from 'bootstrap-vue';
 import './scss/custom.scss';
 
-Vue.use(BootstrapVue);
+Vue.component('b-container', BContainer);
+Vue.component('b-row', BRow);
+Vue.component('b-col', BCol);
+Vue.component('b-navbar', BNavbar);
+Vue.component('b-form-checkbox', BFormCheckbox);
+Vue.use(ModalPlugin);
+Vue.component('b-link', BLink);
+Vue.component('b-form-radio-group', BFormRadioGroup);
+Vue.component('b-card', BCard);
+Vue.component('b-card-body', BCardBody);
+Vue.component('b-card-title', BCardTitle);
+Vue.component('b-table', BTable);
+Vue.component('b-button', BButton);
 
 import { Icon } from 'leaflet';
 
@@ -44,7 +70,7 @@ delete Icon.Default.prototype._getIconUrl;
 Icon.Default.mergeOptions({
   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
   iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
 import 'leaflet/dist/leaflet.css';
@@ -54,5 +80,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App)
 }).$mount('#app');
